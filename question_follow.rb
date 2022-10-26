49af16026fab33a62db7fbf7adf0eb62bbff772d
HishamElmorsi7 <hi7u3xe@gmail.com>
2022-10-26 02:06:29 +0200

    Initial question_follow model

---
+++ question_follow.rb
@@ -0,0 +1,25 @@
+require_relative 'DBConnection'
+
+class Question_follow
+    attr_accessor :id, :user, :question
+
+    def initialize(options)
+        @id = options['id']
+        @user_id = options['user_id']
+        @question_id = options['question_id']
+    end
+
+    def self.find_by_id(id)
+        question_follow = DBConnection.instance.execute(<<~SQL, id)
+            SELECT
+                *
+            FROM
+                question_follows
+            WHERE
+                id = ?
+        SQL
+
+        Question_follow.new(question_follow.first) unless question_follow.empty?
+    end
+
+end
