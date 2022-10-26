3df53188426350229985615fce2c3bad938188e9
HishamElmorsi7 <hi7u3xe@gmail.com>
2022-10-26 02:10:49 +0200

    Initial question_like model

---
+++ question_like.rb
@@ -0,0 +1,25 @@
+require_relative 'DBConnection'
+
+class Question_like
+    attr_accessor :id, :user, :question
+
+    def initialize(options)
+        @id = options['id']
+        @user_id = options['user_id']
+        @question_id = options['question_id']
+    end
+
+    def self.find_by_id(id)
+        question_like = DBConnection.instance.execute(<<~SQL, id)
+            SELECT
+                *
+            FROM
+                question_likes
+            WHERE
+                id = ?
+        SQL
+
+        Question_like.new(question_like.first) unless question_like.empty?
+    end
+
+end
